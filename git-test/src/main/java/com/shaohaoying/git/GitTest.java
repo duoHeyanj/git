db3dc0a49edca0930269e3525e854529676b00c4
chengang <2396407537@qq.com>
2021-12-03 09:56:27 +0800

    second commit

--- git-test/src/main/java/com/shaohaoying/git/GitTest.java
+++ git-test/src/main/java/com/shaohaoying/git/GitTest.java
@@ -3,5 +3,9 @@ package com.shaohaoying.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("邵好英傻逼");
+
+        System.out.println("邵好英250");
     }
+
+
 }
